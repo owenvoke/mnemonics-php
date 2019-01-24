89963fca702804f523ba15b81908b2b2ef163016
pxgamer <owzie123@gmail.com>
2019-01-24 17:34:58 +0000

    Add bit array and mnemonic generator classes

---
+++ src/BitArray.php
@@ -0,0 +1,135 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pxgamer\Mnemonics;
+
+class BitArray implements \ArrayAccess, \Countable, \Iterator
+{
+    private $bits = [];
+
+    public function __construct($bytes)
+    {
+        if (is_string($bytes)) {
+            $this->bits = self::bytesToBits($bytes);
+        }
+
+        if (is_array($bytes)) {
+            // Check all bytes are actually 1 or 0
+            $invalidBytes = array_filter($bytes, function ($v) {
+                return ($v > 1) || ($v < 0);
+            });
+
+            if (count($invalidBytes) > 0) {
+                throw new \InvalidArgumentException('Array argument contains values other than 0,1,true,false');
+            }
+
+            $this->bits = $bytes;
+        }
+    }
+
+    public function toBytes(): string
+    {
+        return $this->__toString();
+    }
+
+    public function __toString(): string
+    {
+        $str = '';
+
+        for ($i = 0; $i < count($this->bits) / 8; $i++) {
+            $slice = array_slice($this->bits, $i * 8, 8);
+            $index = 0;
+            for ($j = 0; $j < count($slice); $j++) {
+                $index <<= 1;
+                if ($slice[$j]) {
+                    $index |= 0x01;
+                }
+            }
+            $str .= chr($index);
+        }
+
+        return $str;
+    }
+
+    private static function bytesToBits($bytes): array
+    {
+        $bits = [];
+        $numBytes = strlen($bytes);
+
+        for ($i = 0; $i < $numBytes; $i++) {
+            $byte = ord($bytes[$i]);
+            for ($j = 7; $j >= 0; $j--) {
+                $idx = 0x01 << $j;
+                $bits[] = (($idx & $byte) === $idx);
+            }
+        }
+
+        return $bits;
+    }
+
+    public function offsetExists($offset): bool
+    {
+        return array_key_exists($offset, $this->bits);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->bits[$offset];
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        $this->bits[$offset] = $value;
+    }
+
+    public function offsetUnset($offset): void
+    {
+        unset($this->bits[$offset]);
+    }
+
+    public function count(): int
+    {
+        return count($this->bits);
+    }
+
+    public function slice($offset, $length): self
+    {
+        return new self(array_slice($this->bits, $offset, $length));
+    }
+
+    public function current()
+    {
+        return current($this->bits);
+    }
+
+    public function next()
+    {
+        return next($this->bits);
+    }
+
+    public function key()
+    {
+        return key($this->bits);
+    }
+
+    public function valid(): bool
+    {
+        return $this->key() !== null;
+    }
+
+    public function rewind(): void
+    {
+        reset($this->bits);
+    }
+
+    public function merge(BitArray $bitArray2): self
+    {
+        return new self(array_merge($this->bits, $bitArray2->toArray()));
+    }
+
+    public function toArray(): array
+    {
+        return $this->bits;
+    }
+}
---
+++ src/Mnemonic.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pxgamer\Mnemonics;
+
+class Mnemonic
+{
+    public const ENTROPY_BITS = 128;
+
+    private $words;
+
+    public function __construct(array $wordList)
+    {
+        if (count($wordList) !== 2048) {
+            throw new \RuntimeException('Invalid number of mnemonic words provided. Must be 2048.');
+        }
+
+        $this->words = $wordList;
+    }
+
+    public function toMnemonic(string $entropy): array
+    {
+        // Entropy should be in 32 bit (4 byte) multiples
+        if (strlen($entropy) % 4 !== 0) {
+            throw new \RuntimeException('Entropy bits must be divisible by 32');
+        }
+
+        $hash = hash('sha256', $entropy, true);
+        $entropyBits = new BitArray($entropy);
+        $hashBits = new BitArray($hash);
+        $checksumLen = count($entropyBits) / 32;
+
+        $concatBits = $entropyBits->merge($hashBits->slice(0, $checksumLen));
+
+        // Split into 11 bit chunks, encoding numbers 0-2047, as index spots in a word list
+        $words = [];
+        $numWords = count($concatBits) / 11;
+        for ($i = 0; $i < $numWords; $i++) {
+            $slice = $concatBits->slice($i * 11, 11);
+            $index = 0;
+            for ($j = 0; $j < count($slice); $j++) {
+                $index <<= 1;
+                if ($slice[$j]) {
+                    $index |= 0x01;
+                }
+            }
+
+            $words[] = $this->words[$index];
+        }
+
+        return $words;
+    }
+
+    public function toEntropy(array $words): string
+    {
+        // Collect up words, compact into bit array
+        $temporaryBitArray = new BitArray([]);
+
+        for ($i = 0; $i < count($words); $i++) {
+            $wordIndex = array_search($words[$i], $this->words, true);
+            for ($j = 0; $j < 11; $j++) {
+                $temporaryBitArray[($i * 11) + $j] = (($wordIndex & (1 << 10 - $j)) !== 0);
+            }
+        }
+
+        $concatLenBits = count($temporaryBitArray);
+        $checksumLengthBits = $concatLenBits / 33;
+        $entropyLengthBits = $concatLenBits - $checksumLengthBits;
+
+        $entropy = $temporaryBitArray->slice(0, $entropyLengthBits)->toBytes();
+        $hashBits = new BitArray(hash('sha256', $entropy, true));
+        $checksum = $temporaryBitArray->slice($entropyLengthBits, $checksumLengthBits);
+
+        if ($checksum->toArray() !== $hashBits->slice(0, $checksumLengthBits)->toArray()) {
+            throw new \RuntimeException('Invalid checksum');
+        }
+
+        return $entropy;
+    }
+}
